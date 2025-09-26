7627f90dcabf9b203c7c94258e0cbbf3a3650792
Christopher Mortimer <christopher@mortimer.xyz>
2025-09-26 11:26:42 +1000

    fix: :bug: handle utf8

--- eng/pytpt/ddl/venue.sql
+++ eng/pytpt/ddl/venue.sql
@@ -1,10 +1,10 @@
 create table
   VENUE (
     VENUE_ID integer not null
-  , VENUE_NAME varchar(50) character set LATIN not CASESPECIFIC
+  , VENUE_NAME varchar(50) character set unicode
   , CAPACITY integer
-  , SURFACE varchar(50) character set LATIN not CASESPECIFIC
-  , VENUE_TYPE varchar(50) character set LATIN not CASESPECIFIC
+  , SURFACE varchar(50) character set unicode
+  , VENUE_TYPE varchar(50) character set unicode
   , constraint VENUE_ID_PK primary key (VENUE_ID)
   )
 ;
